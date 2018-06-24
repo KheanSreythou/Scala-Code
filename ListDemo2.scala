64e172c5317eeff0da0aae7e990f98dfbb8e07ea
Sreythou Khean <kheansreythou19@gmail.com>
2018-06-24 16:04:55 +0700

    Repeats List

---
+++ ListDemo2.scala
@@ -0,0 +1,9 @@
+object ListDemo2{
+  def main(args:Array[String]){
+    val fruit=List.fill(3)("Apple")
+    println("Fruit:"+fruit)
+    val num=List.fill(10)(2)
+    println("Number: "+num)
+  }
+}
+    
