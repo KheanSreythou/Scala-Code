d1da8072abb97266e9d23bc479b8f8672e6784ff
Sreythou Khean <kheansreythou19@gmail.com>
2018-06-25 13:25:33 +0700

    Code to  print the fibonacci number.

--- FibonacciDemo.scala
+++ FibonacciDemo.scala
@@ -3,9 +3,9 @@ object FibonacciDemo {
     println("Input number:")
     var num=scala.io.StdIn.readInt
     println("Fibonacci of "+num+" : ")
-    print(fibo(num))
+    print(fib(num))
   }
-  def fibo(n:Int){
+  def fib(n:Int){
     var temp:Int=0; var a:Int=0
     var b:Int=1
     print(a+"\t")
