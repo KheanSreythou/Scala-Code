d879b8484ae3ed327117acef11252cdce316e003
Sreythou Khean <kheansreythou19@gmail.com>
2018-06-25 12:01:29 +0700

    Make a list of list.

--- ListOfListDemo.scala
+++ ListOfListDemo.scala
@@ -2,7 +2,6 @@ object ListOfList{
   def main(args:Array[String]){
     var lists: List[List[String]]=List(List("Dave","John","Will"),List("Kerry","Merrice","Lavice"))
     println(lists)
-  
   }
 }
 
